2bfe3e268c74634a236bf8c721f5d4eee4f91942
DavidBautista <davidalejandrobautistaramos@gmail.com>
2024-11-20 08:05:20 -0500

    token en login

--- src/pages/home/CreatePost.jsx
+++ src/pages/home/CreatePost.jsx
@@ -23,7 +23,6 @@ const CreatePost = () => {
 					method: "POST",
 					headers: { "Content-Type": "application/json" },
 					body: JSON.stringify({ contenido, fotoPublicacion }),
-					withCredentials: true, // Asegúrate de incluir las credenciales (cookies)
 				});
 				const data = await res.json();
 				if (!res.ok) throw new Error(data.error || "Algo salió mal");
